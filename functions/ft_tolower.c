021aa88a3b89149da9e068ce41ceab14bdc09850
Jorge Luiz De Carvalho Silva <jluiz-de@c1r10s3.42lisboa.com>
2024-10-28 19:58:04 +0000

    Add tolower and toupper

---
+++ functions/ft_tolower.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jluiz-de <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 19:35:39 by jluiz-de          #+#    #+#             */
+/*   Updated: 2024/10/28 19:55:28 by jluiz-de         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_tolower(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return c + 32;
+	else
+		return (c);
+}
+#include <stdio.h>
+int	main()
+{
+	char	c1 = 'a';
+	char	c2 = 'A';
+	char	c3 = '1';
+
+	printf("%c\n", ft_tolower(c1));
+	printf("%c\n", ft_tolower(c2));
+	printf("%c\n", ft_tolower(c3));
+
+	return(0);
+}
---
+++ functions/ft_toupper.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jluiz-de <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 19:35:39 by jluiz-de          #+#    #+#             */
+/*   Updated: 2024/10/28 19:50:34 by jluiz-de         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_toupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return c - 32;
+	else
+		return (c);
+}
+/*#include <stdio.h>
+int	main()
+{
+	char	c1 = 'a';
+	char	c2 = 'A';
+	char	c3 = '1';
+
+	printf("%c\n", ft_toupper(c1));
+	printf("%c\n", ft_toupper(c2));
+	printf("%c\n", ft_toupper(c3));
+
+	return(0);
+}*/
