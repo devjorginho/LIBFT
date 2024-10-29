10435d7f2463514b44a34f5afe25b06087818db1
Jorge Luiz De Carvalho Silva <jluiz-de@c2r7s5.42lisboa.com>
2024-10-29 18:05:57 +0000

    mem exs

---
+++ functions/ft_memset.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jluiz-de <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 16:31:34 by jluiz-de          #+#    #+#             */
+/*   Updated: 2024/10/29 17:15:44 by jluiz-de         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+void	*ft_memset(void *s, int c, size_t n)
+{
+	if (n <= 0)
+		return (s);
+	*(unsigned char *)s = (unsigned char)c;
+	ft_memset((unsigned char *)s + 1, c, n - 1);
+	return(s);
+}
+/*#include<stdio.h>
+int	main()
+{
+	char array1[] = "Paulim";
+	
+	ft_memset(array1, 'A', 2);
+	printf("%s\n", array1);
+	return(0);
+}*/
